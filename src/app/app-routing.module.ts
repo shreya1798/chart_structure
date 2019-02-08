6423327953b66f40a149bc2f508d2ce156fa02d1
shreya1798 <shreyasgnr@gmail.com>
2019-02-08 11:50:40 +0530

    firstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TreeComponent } from './tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: TreeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
