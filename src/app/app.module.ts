82aeaa45083d6eff76255cb9eee2ce66cb277b6d
AnandRoy01 <anandroyyo@gmail.com>
2019-02-12 13:53:18 +0530

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreeComponent } from './tree/tree.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import {MatCardModule} from '@angular/material/card';
 import { NavComponent } from './nav/nav.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
@@ -25,7 +25,9 @@ import { PopupComponent } from './popup/popup.component';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
